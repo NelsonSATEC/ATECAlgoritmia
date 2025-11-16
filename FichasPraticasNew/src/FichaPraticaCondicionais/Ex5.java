2a8abffb77657d6ff6e1627412238defa464befb
NelsonSATEC <nelson.sousa.T0133732@edu.atec.pt>
2025-11-16 17:47:04 +0000

    Casa

--- FichasPraticasNew/src/FichaPraticaCondicionais/Ex5.java
+++ FichasPraticasNew/src/FichaPraticaCondicionais/Ex5.java
@@ -8,6 +8,27 @@ public class Ex5 {
 
         Scanner input=new Scanner(System.in);
 
+        int numerocerto, tentativa1, tentativa2;
+        
+        numerocerto=31;
+
+        System.out.println("Adivinhe o número de 0 a 100: ");
+        tentativa1= input.nextInt();
+        
+        if (tentativa1==31) {
+            System.out.println("Acertou!");
+        }else if (tentativa1<31){
+            System.out.println("O número é muito baixo, tem mais uma tentativa");
+        } else if (tentativa1>31) {
+            System.out.println("O número é muito alto, tem mais uma tentativa");
+        }
+        tentativa2= input.nextInt();
+
+        if (tentativa2==31) {
+            System.out.println("Acertou!");
+        } else if (tentativa2!=31) {
+            System.out.println("Falhou, acabaram as tentativas.");
+        }
 
 
     }
